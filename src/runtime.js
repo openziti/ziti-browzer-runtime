baea9886bc4bc0c245c5e779ae3bb6e07a293aa9
Curt Tudor <curt@rentallect.com>
2023-07-19 08:58:26 -0600

    emit troubleshooting alerts

--- src/runtime.js
+++ src/runtime.js
@@ -842,7 +842,83 @@ class ZitiBrowzerRuntime {
 
     this.logger.trace(`noConfigForServiceEventHandler() `, noConfigForServiceEvent);
 
-    let errStr = `Ziti Service [${noConfigForServiceEvent.serviceName}] has no associated configs.\n\nPossible Ziti network misconfiguration exists.`;
+    let errStr = `Ziti Service [${noConfigForServiceEvent.serviceName}] has no associated configs.\n\nContact your Ziti Network admin.`;
+
+    alert(errStr);
+
+  }
+
+  noServiceEventHandler(noServiceEvent) {
+
+    this.logger.trace(`noServiceEventHandler() `, noServiceEvent);
+
+    var distance = function (a, b) {
+      var _a;
+      if (a.length === 0)
+          return b.length;
+      if (b.length === 0)
+          return a.length;
+      if (a.length > b.length)
+          _a = [b, a], a = _a[0], b = _a[1];
+      var row = [];
+      for (var i = 0; i <= a.length; i++)
+          row[i] = i;
+      for (var i = 1; i <= b.length; i++) {
+          var prev = i;
+          for (var j = 1; j <= a.length; j++) {
+              var val = void 0;
+              if (b.charAt(i - 1) === a.charAt(j - 1))
+                  val = row[j - 1];
+              else
+                  val = Math.min(row[j - 1] + 1, prev + 1, row[j] + 1);
+              row[j - 1] = prev;
+              prev = val;
+          }
+          row[a.length] = prev;
+      }
+      return row[a.length];
+    };
+
+    var closestMatch = function (target, array, showOccurrences) {
+      if (showOccurrences === void 0) { showOccurrences = false; }
+      if (array.length === 0)
+          return null;
+      var vals = [];
+      var found = [];
+      for (var i = 0; i < array.length; i++)
+          vals.push((0, distance)(target, array[i]));
+      var min = Math.min.apply(Math, vals);
+      for (var i = 0; i < vals.length; i++) {
+          if (vals[i] === min)
+              found.push(array[i]);
+      }
+      return showOccurrences ? found : found[0];
+    };  
+  
+
+    let cm = closestMatch(noServiceEvent.serviceName, noServiceEvent.serviceList, true);
+
+    let errStr = `Ziti Service [${noServiceEvent.serviceName}] not found in list of Services your Identity can access -- closest match [${cm}].\n\nContact your Ziti Network admin.`;
+
+    alert(errStr);
+
+  }
+
+  sessionCreationErrorEventHandler(sessionCreationErrorEvent) {
+
+    this.logger.trace(`sessionCreationErrorEventHandler() `, sessionCreationErrorEvent);
+
+    let errStr = `Ziti Service [${window.zitiBrowzerRuntime.zitiConfig.httpAgent.target.service}] cannot be reached -- [${sessionCreationErrorEvent.error}].\n\nContact your Ziti Network admin.`;
+
+    alert(errStr);
+
+  }
+
+  invalidAuthEventHandler(invalidAuthEvent) {
+
+    this.logger.trace(`invalidAuthEventHandler() `, invalidAuthEvent);
+
+    let errStr = `User [${invalidAuthEvent.email}] cannot be authenticated onto Ziti Network.\n\nContact your Ziti Network admin.`;
 
     alert(errStr);
 
@@ -1326,8 +1402,11 @@ class ZitiBrowzerRuntime {
 
       this.zitiContext.listControllerVersion();
 
-      this.zitiContext.on('idpAuthHealthEvent',       this.idpAuthHealthEventHandler);
-      this.zitiContext.on('noConfigForServiceEvent',  this.noConfigForServiceEventHandler);
+      this.zitiContext.on('idpAuthHealthEvent',         this.idpAuthHealthEventHandler);
+      this.zitiContext.on('noConfigForServiceEvent',    this.noConfigForServiceEventHandler);
+      this.zitiContext.on('noServiceEvent',             this.noServiceEventHandler);
+      this.zitiContext.on('sessionCreationErrorEvent',  this.sessionCreationErrorEventHandler);
+      this.zitiContext.on('invalidAuthEvent',           this.invalidAuthEventHandler);
       this.zitiContext.on(ZITI_CONSTANTS.ZITI_EVENT_XGRESS, this.xgressEventHandler);
 
     }
@@ -1768,6 +1847,24 @@ if (isUndefined(window.zitiBrowzerRuntime)) {
         else if (event.data.type === 'NO_CONFIG_FOR_SERVICE') {
 
           window.zitiBrowzerRuntime.noConfigForServiceEventHandler(event.data.payload.event);
+
+        }
+
+        else if (event.data.type === 'SESSION_CREATION_ERROR') {
+
+          window.zitiBrowzerRuntime.sessionCreationErrorEventHandler(event.data.payload.event);
+
+        }
+
+        else if (event.data.type === 'NO_SERVICE') {
+
+          window.zitiBrowzerRuntime.noServiceEventHandler(event.data.payload.event);
+
+        }
+
+        else if (event.data.type === 'INVALID_AUTH') {
+
+          window.zitiBrowzerRuntime.invalidAuthEventHandler(event.data.payload.event);
 
         }
 

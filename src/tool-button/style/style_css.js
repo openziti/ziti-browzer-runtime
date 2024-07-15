38cba29d24a5a85a1ec20c7c5a5d7a21e190791f
Curt Tudor <curt@rentallect.com>
2024-07-15 10:55:28 -0600

    mobile adjustments for throughput chart

--- src/tool-button/style/style_css.js
+++ src/tool-button/style/style_css.js
@@ -7,24 +7,37 @@ const STYLE_CSS =
 
 `
 #eruda-info{overflow-y:auto;-webkit-overflow-scrolling:touch}#eruda-info li{margin:10px;border:1px solid #423f43}#eruda-info li .eruda-content,#eruda-info li .eruda-title{padding:10px}#eruda-info li .eruda-title{position:relative;padding-bottom:0;color:#ffd866}#eruda-info li .eruda-title .eruda-icon-copy{position:absolute;right:10px;top:14px;color:#939293;cursor:pointer;transition:color .3s}#eruda-info li .eruda-title .eruda-icon-copy:active{color:#ffd866}#eruda-info li .eruda-content{margin:0;-webkit-user-select:text;-moz-user-select:text;-ms-user-select:text;user-select:text;color:#fcfcfa;font-size:12px;word-break:break-all}#eruda-info li .eruda-content table{width:100%;border-collapse:collapse}#eruda-info li .eruda-content table td,#eruda-info li .eruda-content table th{border:1px solid #423f43;padding:10px}#eruda-info li .eruda-content *{-webkit-user-select:text;-moz-user-select:text;-ms-user-select:text;user-select:text}#eruda-info li .eruda-content a{color:#78DCE8}#eruda-info li .eruda-device-key,#eruda-info li .eruda-system-key{width:100px}.eruda-safe-area #eruda-info{padding-bottom:calc(10px + env(safe-area-inset-bottom))}
+
+
+.OpenZiti-BrowZer-splash-screen {
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  opacity: 1;
+  transition: opacity 0.5s ease-in-out;
+  font-family: Metropolis, sans-serif;
+  font-size: 10px;
+  font-weight: 600;
+}
+
+
+.OpenZiti-BrowZer-loader {
+  border: 4px solid #f3f3f3;
+  border-top: 4px solid #3498db;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  animation: spin 1s linear infinite;
+  margin: 20px auto;
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+
+
 `
 
-// .zitiBrowzerRuntimeCursor {
-//   position: absolute;
-//   width: 20px;
-//   height: 20px;
-//   border-radius: 50%;
-//   background-color: red; /* Example cursor color */
-//   pointer-events: none;
-//   z-index: 9999;
-// }
-// .zitiBrowzerRuntimeTooltip {
-//   position: absolute;
-//   display: none;
-//   padding: 5px;
-//   background-image: linear-gradient(to right, #0965f3, #e10c5c) !important;
-//   color: white;
-//   border-radius: 5px;
-//   pointer-events: none;
-//   z-index: 9999;
-// }

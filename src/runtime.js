67a54386afc398dfc57425c2462c0fd73c94d66e
Curt Tudor <curt@rentallect.com>
2025-02-25 14:56:26 -0700

    feat: security updates (#424)

--- src/runtime.js
+++ src/runtime.js
@@ -37,7 +37,6 @@ import { buildInfo } from './buildInfo'
 import { ZitiBrowzerLocalStorage } from './utils/localstorage';
 import { Auth0Client } from '@auth0/auth0-spa-js';
 import Bowser from 'bowser'; 
-import * as msal from '@azure/msal-browser';
 import { stringify } from './urlon';
 import * as oidc from 'oauth4webapi';
 import {
@@ -1041,110 +1040,6 @@ class ZitiBrowzerRuntime {
     let check = false;
     (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
     return check;
-  }
-  
-  /**
-   * Instantiate the IdP auth client
-   * 
-   *  TODO: delete all this once we have tested the new OIDC support with AzureAD
-   */
-  async authClient_instantiate() {
-
-    /**
-     *  Identify the IdP
-     */
-    if ( (this.zitiConfig.idp.host.match( regexAuth0URL )) ) {
-      this.idp = ZBR_CONSTANTS.AUTH0_IDP
-    }
-    else if ( (this.zitiConfig.idp.host.match( regexAzureADURL )) ) {
-      this.idp = ZBR_CONSTANTS.AZURE_AD_IDP
-    }
-    else {
-
-      window.zitiBrowzerRuntime.browzer_error({
-        status:   409,
-        code:     ZBR_CONSTANTS.ZBR_ERROR_CODE_INVALID_IDP_CONFIG,
-        title:    `Invalid IdP config [${JSON.stringify(this.zitiConfig.idp)}]`,
-        message:  `Cannot identify the specified IdP type`
-      });
-  
-    }
-
-    /**
-     *  Instantiate the appropriate client
-     */
-    if ( isEqual(this.idp, ZBR_CONSTANTS.KEYCLOAK_IDP) ) {
-
-      this.authClient = new Keycloak({
-        url: this.zitiConfig.idp.host,
-        realm: this.zitiConfig.idp.realm,
-        clientId: this.zitiConfig.idp.clientId
-      });
-    
-    }
-
-    else if ( isEqual(this.idp, ZBR_CONSTANTS.AUTH0_IDP) ) {
-
-      this.authClient = new Auth0Client({
-        domain:   this.zitiConfig.idp.host,
-        clientId: this.zitiConfig.idp.clientId,
-        cacheLocation: 'localstorage', // valid values are: 'memory' or 'localstorage',
-        authorizationParams: {
-          redirect_uri: `${window.location.origin}`
-        }    
-      });
-
-    } else if ( isEqual(this.idp, ZBR_CONSTANTS.AZURE_AD_IDP) ) {
-
-      let msalConfig = {
-
-        auth: {  
-          clientId:     `${this.zitiConfig.idp.clientId}`,
-          authority:    `${this.zitiConfig.idp.host}`,
-          redirectUri:  `${window.location.origin}`,
-          postLogoutRedirectUri: `${window.location.origin}`,
-        },
-        cache: {
-            cacheLocation: "sessionStorage",
-            storeAuthStateInCookie: false,
-        },
-        system: {
-            loggerOptions: {
-                loggerCallback: (level, message, containsPii) => {
-                    if (containsPii) {
-                        return;
-                    }
-                    switch (level) {
-                        case msal.LogLevel.Error:
-                            window.zitiBrowzerRuntime.logger.error(`${message}`);
-                            return;
-                        case msal.LogLevel.Info:
-                            window.zitiBrowzerRuntime.logger.info(`${message}`);
-                            return;
-                        case msal.LogLevel.Verbose:
-                            window.zitiBrowzerRuntime.logger.debug(`${message}`);
-                            return;
-                        case msal.LogLevel.Warning:
-                            window.zitiBrowzerRuntime.logger.warn(`${message}`);
-                            return;
-                        default:
-                            window.zitiBrowzerRuntime.logger.info(`${message}`);
-                            return;
-                    }
-                }
-            }
-          }
-      };
-
-      window.zitiBrowzerRuntime.authClient = new msal.PublicClientApplication(msalConfig);
-      window.zitiBrowzerRuntime.authClient.initialize().then(() => {
-        window.zitiBrowzerRuntime.authClient.handleRedirectPromise().then(window.zitiBrowzerRuntime.handleAADRedirectResponse).catch(error => {
-          window.zitiBrowzerRuntime.logger.error(`${error}`);
-        });
-      });
-              
-    }
-
   }
 
   handleAADRedirectResponse(resp) {
